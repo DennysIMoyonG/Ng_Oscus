e3aabe05bd94c091eaf4f9cc6ec2fa523dad7623
Dennys Moyon <dennysmoyon@outlook.com>
2021-08-18 20:56:54 -0500

    clase numero 7, bootstrap, grid system, direcivas, binding event and property

--- src/app/componentes/mostrar-usuario/mostrar-usuario.component.ts
+++ src/app/componentes/mostrar-usuario/mostrar-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mostrar-usuario',
@@ -13,6 +13,15 @@ export class MostrarUsuarioComponent implements OnInit, OnDestroy {
   objeto:any = {nombre:'dennys'};
   arreglo = [2,3,4,2];
 
+  @Input() 
+  usuario = {
+    nombre : "Dennys Moyón",
+    imagen : "https://www.adslzone.net/app/uploads-adslzone.net/2021/01/whatsapp-cambiar-perfil.jpg",
+    descripcion : "WhatsApp sigue a la cabeza de las aplicaciones de mensajería instantánea con más de 1.600 millones de usuarios activos mensuales en todo el mundo. La herramienta, propiedad de Facebook, nos permite editar nuestro perfil añadiendo una foto, un nombre e información sobre nosotros."
+  }
+
+  
+
   constructor() { }
   ngOnDestroy(): void {
     // limpiar variables, observables
@@ -23,4 +32,15 @@ export class MostrarUsuarioComponent implements OnInit, OnDestroy {
     console.log("Iniciar MostrarUsuarioComponent")
   }
 
+  cambiarImagen(evento:any, usuario:any){
+    console.log("evento", evento);
+    console.log("usuario", usuario);
+    
+    let auxiliar = "https://as.com/meristation/imagenes/2021/02/15/noticias/1613386928_932921_1613391944_noticia_normal.jpg";
+    if(this.usuario.imagen == auxiliar){
+      this.usuario.imagen = "https://www.adslzone.net/app/uploads-adslzone.net/2021/01/whatsapp-cambiar-perfil.jpg";
+    }else{
+      this.usuario.imagen = auxiliar;
+    }
+  }
 }
--- src/app/rutas/ruta-login/ruta-login.component.ts
+++ src/app/rutas/ruta-login/ruta-login.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaLoginComponent implements OnInit {
 
+  mostarPrimerComponente:boolean = true;
+  usuarios = [
+    {
+      nombre : "Edwin Moyón",
+      imagen : "https://www.leisa-al.org/web/images/stories/noticias/2017/diciembre/arbol1.jpg",
+      descripcion : "Te comparto que, en cuanto tengas 50 puntos de reputación, podrás realizar comentarios. Creo que tu respuesta es correcta, pero fue detectada como una publicación de baja calidad por su poco contenido. En todo caso, te agradezco por tu aporte. Saludo."
+    },
+    {
+      nombre : "Nelson Moyón",
+      imagen : "https://img.fotocommunity.com/maigruen-9082d97b-ac4e-481e-a726-12c9faa10af8.jpg?height=400",
+      descripcion : "descripcion 3"
+    },
+    {
+      nombre : "Cristhian Moyón",
+      imagen : "https://www.jardineriaon.com/wp-content/uploads/2017/07/arboles-en-alemania.jpg",
+      descripcion : "descripcion 4"
+    },
+    {
+      nombre : "Jhonatan Moyón",
+      imagen : "https://image.slidesharecdn.com/arboles-131106080436-phpapp01/95/arboles-1-638.jpg?cb=1383725104",
+      descripcion : "descripcion 5"
+    }
+  
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
